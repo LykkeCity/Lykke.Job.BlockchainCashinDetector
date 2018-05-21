8dce1238698a200a3d0b75c329a0d397cd57bf07
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-05-21 22:51:59 +0300

    LWDEV-7307 tests base

---
+++ tests/Lykke.Job.BlockchainCashinDetector.Tests/Integration/Settings/BlockchainCashinDetectorIntegrationTestsSettings.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Lykke.Job.BlockchainCashinDetector.Tests.Integration.Settings
+{
+    [UsedImplicitly]
+    public class BlockchainCashinDetectorIntegrationTestsSettings
+    {
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
+        public string AzureStorageConnectionString { get; set; }
+
+        [UsedImplicitly(ImplicitUseKindFlags.Assign)]
+        public string RabbitMqConnectionString { get; set; }
+    }
+}
