2372fb4a74ef8036e61ea4d3be6af41a66b9bd0f
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-08-08 22:54:36 +0300

    LWDEV-8726 Merge conflict fix.

--- src/Lykke.Job.BlockchainCashinDetector/Workflow/CommandHandlers/NotifyCashinCompletedCommandsHandler.cs
+++ src/Lykke.Job.BlockchainCashinDetector/Workflow/CommandHandlers/NotifyCashinCompletedCommandsHandler.cs
@@ -21,8 +21,8 @@ namespace Lykke.Job.BlockchainCashinDetector.Workflow.CommandHandlers
             {
                 ClientId = command.ClientId,
                 AssetId = command.AssetId,
-                Amount = command.Amount,
                 OperationId = command.OperationId,
+                Amount = command.OperationAmount,
                 TransactionHash = command.TransactionHash
             });
 
