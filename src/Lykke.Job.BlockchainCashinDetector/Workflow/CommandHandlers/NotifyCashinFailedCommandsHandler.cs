e134d98c74d212439d5dac7e7c806d507493c213
giorgi bakuridze <bakuridze13@gmail.com>
2018-09-02 15:00:11 +0400

    Add OperationExecution error code process in OperationExecutionFailedEvent

---
+++ src/Lykke.Job.BlockchainCashinDetector/Workflow/CommandHandlers/NotifyCashinFailedCommandsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Lykke.Cqrs;
+using Lykke.Job.BlockchainCashinDetector.Contract.Events;
+using Lykke.Job.BlockchainCashinDetector.Workflow.Commands;
+
+namespace Lykke.Job.BlockchainCashinDetector.Workflow.CommandHandlers
+{
+    public class NotifyCashinFailedCommandsHandler
+    {
+        [UsedImplicitly]
+        public Task<CommandHandlingResult> Handle(NotifyCashinFailedCommand command, IEventPublisher publisher)
+        {
+            publisher.PublishEvent(new CashinFailedEvent
+            {
+                ClientId = command.ClientId,
+                AssetId = command.AssetId,
+                Amount = command.Amount,
+                OperationId = command.OperationId,
+                Error = command.Error,
+                ErrorCode = command.ErrorCode
+            });
+
+            return Task.FromResult(CommandHandlingResult.Ok());
+        }
+    }
+}
---
+++ src/Lykke.Job.BlockchainCashinDetector/Workflow/Commands/NotifyCashinFailedCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Job.BlockchainCashinDetector.Contract;
+using MessagePack;
+
+namespace Lykke.Job.BlockchainCashinDetector.Workflow.Commands
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class NotifyCashinFailedCommand
+    {
+        public Guid ClientId { get; set; }
+        public decimal Amount { get; set; }
+        public string AssetId { get; set; }
+        public Guid OperationId { get; set; }
+        public string Error { get; set; }
+        public CashinErrorCode ErrorCode { get; set; }
+    }
+}
