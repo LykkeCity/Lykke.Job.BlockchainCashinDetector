b29be00792be4db85c8a9f09c5e7cdd2bffe2f69
AleksandrBelousov <tortyt1@gmail.com>
2019-02-11 19:57:40 +0400

    temp fix in dev TODO: Cherry pick after risk management in test

--- src/Lykke.Job.BlockchainCashinDetector/Modules/JobModule.cs
+++ src/Lykke.Job.BlockchainCashinDetector/Modules/JobModule.cs
@@ -7,6 +7,7 @@ using Lykke.Job.BlockchainCashinDetector.Services;
 using Lykke.Job.BlockchainCashinDetector.Settings;
 using Lykke.Job.BlockchainCashinDetector.Workflow.Commands;
 using Lykke.Job.BlockchainCashinDetector.Workflow.Events;
+using Lykke.Job.BlockchainRiskControl.Contract.Events;
 using Lykke.Sdk;
 using Lykke.Service.Assets.Client;
 using Lykke.SettingsReader;
@@ -26,48 +27,56 @@ namespace Lykke.Job.BlockchainCashinDetector.Modules
         protected override void Load(ContainerBuilder builder)
         {
             Lykke.Cqrs.MessageCancellation.Configuration.ContainerBuilderExtensions.RegisterCqrsMessageCancellation(
-            builder,
-            (options) =>
-            {
-                #region Registry
+                builder,
+                (options) =>
+                {
+                    #region Registry
 
-                options.Value
-                    //Commands
-                    .MapMessageId<RetrieveClientCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<EnrollToMatchingEngineCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<NotifyCashinCompletedCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<NotifyCashinFailedCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<ReleaseDepositWalletLockCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<ResetEnrolledBalanceCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<SetEnrolledBalanceCommand>(x => x.OperationId.ToString())
-                    .MapMessageId<LockDepositWalletCommand>(x => x.BlockchainType.ToString())
+                    options.Value
+                        //Commands
+                        .MapMessageId<RetrieveClientCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<EnrollToMatchingEngineCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<NotifyCashinCompletedCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<NotifyCashinFailedCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<ReleaseDepositWalletLockCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<ResetEnrolledBalanceCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<SetEnrolledBalanceCommand>(x => x.OperationId.ToString())
+                        .MapMessageId<LockDepositWalletCommand>(x => x.BlockchainType.ToString())
 
-                    //Events
-                    .MapMessageId<ClientRetrievedEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<CashinCompletedEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<CashinFailedEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<CashinEnrolledToMatchingEngineEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<DepositWalletLockedEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<DepositWalletLockReleasedEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<EnrolledBalanceResetEvent>(x => x.OperationId.ToString())
-                    .MapMessageId<EnrolledBalanceSetEvent>(x => x.OperationId.ToString())
+                        //Events
+                        .MapMessageId<ClientRetrievedEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<CashinCompletedEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<CashinFailedEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<CashinEnrolledToMatchingEngineEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<DepositWalletLockedEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<DepositWalletLockReleasedEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<EnrolledBalanceResetEvent>(x => x.OperationId.ToString())
+                        .MapMessageId<EnrolledBalanceSetEvent>(x => x.OperationId.ToString())
 
-                    //External Commands
-                    .MapMessageId<BlockchainOperationsExecutor.Contract.Commands.StartOneToManyOutputsExecutionCommand>(
-                        x => x.OperationId.ToString())
-                    .MapMessageId<BlockchainOperationsExecutor.Contract.Commands.StartOperationExecutionCommand>(
-                        x => x.OperationId.ToString())
+                        //External Commands
+                        .MapMessageId<BlockchainRiskControl.Contract.Commands.ValidateOperationCommand>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<BlockchainOperationsExecutor.Contract.Commands.
+                            StartOneToManyOutputsExecutionCommand>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<BlockchainOperationsExecutor.Contract.Commands.StartOperationExecutionCommand>(
+                            x => x.OperationId.ToString())
 
-                    //External Events
-                    .MapMessageId<BlockchainOperationsExecutor.Contract.Events.OperationExecutionCompletedEvent>(
-                        x => x.OperationId.ToString())
-                    .MapMessageId<BlockchainOperationsExecutor.Contract.Events.OperationExecutionFailedEvent>(
-                        x => x.OperationId.ToString())
-                    .MapMessageId<BlockchainOperationsExecutor.Contract.Events.OneToManyOperationExecutionCompletedEvent>(
-                        x => x.OperationId.ToString());
+                        //External Events
+                        .MapMessageId<BlockchainRiskControl.Contract.Events.OperationRejectedEvent>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<OperationAcceptedEvent>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<BlockchainOperationsExecutor.Contract.Events.OperationExecutionCompletedEvent>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<BlockchainOperationsExecutor.Contract.Events.OperationExecutionFailedEvent>(
+                            x => x.OperationId.ToString())
+                        .MapMessageId<BlockchainOperationsExecutor.Contract.Events.
+                            OneToManyOperationExecutionCompletedEvent>(
+                            x => x.OperationId.ToString());
 
-                #endregion
-            });
+                    #endregion
+                });
 
             builder.RegisterType<StartupManager>()
                 .As<IStartupManager>();
