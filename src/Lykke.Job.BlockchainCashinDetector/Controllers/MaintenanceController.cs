acfee56b5529402ef84c6b076a3dc199dd281f7f
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-02-16 17:14:33 +0300

    LM-718: added mainenance controller and method to sent EnrollToMatchingEngineCommand

---
+++ src/Lykke.Job.BlockchainCashinDetector/Controllers/MaintenanceController.cs
@@ -0,0 +1,24 @@
+﻿using Lykke.Cqrs;
+using Lykke.Job.BlockchainCashinDetector.Modules;
+using Lykke.Job.BlockchainCashinDetector.Workflow.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Job.BlockchainCashinDetector.Controllers
+{
+    [Route("api/maintenance")]
+    public class MaintenanceController : Controller
+    {
+        private readonly ICqrsEngine _cqrsEngine;
+
+        public MaintenanceController(ICqrsEngine cqrsEngine)
+        {
+            _cqrsEngine = cqrsEngine;
+        }
+
+        [HttpPost("commands/EnrollToMatchingEngineCommand")]
+        public async void SendWaitForTransactionEndingCommand([FromBody] EnrollToMatchingEngineCommand command)
+        {
+            _cqrsEngine.SendCommand(command, $"{CqrsModule.Self}.saga", CqrsModule.Self);
+        }
+    }
+}
